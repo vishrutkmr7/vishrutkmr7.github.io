e5bd940362544e3434600d4a501aef79727ff1af
Vishrut Jha <vishrutkmr7@gmail.com>
2023-03-17 01:49:49 +0000

    heroku

--- leetcode data/scrapper.py
+++ leetcode data/scrapper.py
@@ -1,46 +1,46 @@
-# Get Leetcode rank of the given profile
-# profile: https://leetcode.com/vishrutkmr7/
-
 import requests
+from flask import Flask, jsonify
 from bs4 import BeautifulSoup
 
+app = Flask(__name__)
 
+
+@app.route("/my-leetcode-rank")
 def get_rank(profile):
     response = requests.get(profile, timeout=5)
     soup = BeautifulSoup(response.content, "html.parser")
+    rank_class = "ttext-label-1 dark:text-dark-label-1 font-medium"
+    counts_css = "mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1"
 
-    rank = soup.find(
-        "span", {"class": "ttext-label-1 dark:text-dark-label-1 font-medium"}
-    ).text
+    rank = soup.find("span", {"class": rank_class}).text
     easy = soup.find_all(
         "span",
-        {
-            "class": "mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1"
-        },
+        {"class": counts_css},
     )[0].text
     medium = soup.find_all(
         "span",
-        {
-            "class": "mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1"
-        },
+        {"class": counts_css},
     )[1].text
     hard = soup.find_all(
         "span",
-        {
-            "class": "mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1"
-        },
+        {"class": counts_css},
     )[2].text
 
     solved = int(easy) + int(medium) + int(hard)
 
-    return {
-        "rank": rank,
-        "solved": solved,
-        "easy": easy,
-        "medium": medium,
-        "hard": hard,
-    }
+    return jsonify(
+        {
+            "rank": rank,
+            "solved": solved,
+            "easy": easy,
+            "medium": medium,
+            "hard": hard,
+        }
+    )
 
 
 if __name__ == "__main__":
-    res = get_rank("https://leetcode.com/vishrutkmr7/")
+    # res = get_rank("https://leetcode.com/vishrutkmr7/")
+    # json_result = json.dumps(res)
+    # print(json_result)
+    app.run()
