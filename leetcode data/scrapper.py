a74319af269958a38804b5dbc3a6d6e3bc3996cc
Vishrut Jha <vishrutkmr7@gmail.com>
2023-03-17 06:02:18 +0000

    flask

--- leetcode data/scrapper.py
+++ leetcode data/scrapper.py
@@ -5,28 +5,35 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
-@app.route("/my-leetcode-rank")
-def get_rank(profile):
+@app.route("/my-leetcode-rank/<username>", methods=["GET"])
+def get_rank(username):
+    profile = f"https://leetcode.com/{username}/"
     response = requests.get(profile, timeout=5)
     soup = BeautifulSoup(response.content, "html.parser")
     rank_class = "ttext-label-1 dark:text-dark-label-1 font-medium"
     counts_css = "mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1"
 
     rank = soup.find("span", {"class": rank_class}).text
-    easy = soup.find_all(
-        "span",
-        {"class": counts_css},
-    )[0].text
-    medium = soup.find_all(
-        "span",
-        {"class": counts_css},
-    )[1].text
-    hard = soup.find_all(
-        "span",
-        {"class": counts_css},
-    )[2].text
+    easy = int(
+        soup.find_all(
+            "span",
+            {"class": counts_css},
+        )[0].text
+    )
+    medium = int(
+        soup.find_all(
+            "span",
+            {"class": counts_css},
+        )[1].text
+    )
+    hard = int(
+        soup.find_all(
+            "span",
+            {"class": counts_css},
+        )[2].text
+    )
 
-    solved = int(easy) + int(medium) + int(hard)
+    solved = easy + medium + hard
 
     return jsonify(
         {
@@ -40,7 +47,4 @@ def get_rank(profile):
 
 
 if __name__ == "__main__":
-    # res = get_rank("https://leetcode.com/vishrutkmr7/")
-    # json_result = json.dumps(res)
-    # print(json_result)
     app.run()
